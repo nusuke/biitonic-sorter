214c01bcae22bdac794272038f06bddaad9bbdec
nusuke <nusukep@gmail.com>
2021-10-24 18:11:56 +0900

    impl second

--- src/second.rs
+++ src/second.rs
@@ -1,9 +1,27 @@
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+use super::SortOrder;
+
+// 成功時はOK(())を、失敗時はErr(文字列)を返す
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of x is not a power of two. (x.len(): {})",
+            x.len()
+        ))
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
-        sub_sort(x, up)
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
     }
 }
 
@@ -11,8 +29,8 @@ fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], up);
-        sort(&mut x[mid_point..], up);
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
     }
 }
 
@@ -20,7 +38,7 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
-            x.swap(i, mid_point + i)
+            x.swap(i, mid_point + i);
         }
     }
 }
@@ -28,40 +46,82 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 #[cfg(test)]
 mod tests {
     use super::sort;
+    use crate::SortOrder::*;
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11]; // x.len() が2のべき乗になっていない。
+        assert!(sort(&mut x, &Ascending).is_err()); // 戻り値はErr
+    }
 
     #[test]
     fn sort_u32_ascending() {
-        // setup
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
-
-        // act
-        sort(&mut x, true);
-
-        // then
-        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330])
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
-        // setup
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
-
-        // act
-        sort(&mut x, false);
-
-        // then
-        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4])
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 
     #[test]
-    fn sort_string_ascending() {
-        // setup
-        let mut x = vec!["a", "c", "b"];
+    fn sort_str_ascending() {
+        // 文字列のベクタを作り、ソートする
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with"
+            ]
+        );
+    }
 
-        // act
-        sort(&mut x, true);
-
-        // then
-        assert_eq!(x, vec!["a", "b", "c"])
+    #[test]
+    fn sort_str_descending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "with",
+                "no",
+                "memory-efficient",
+                "is",
+                "fast",
+                "and",
+                "Rust",
+                "GC"
+            ]
+        );
     }
 }
