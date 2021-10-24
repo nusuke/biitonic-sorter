f98af7d70875b8ab530844bcec9e25c1e34d1d35
nusuke <nusukep@gmail.com>
2021-10-24 17:31:47 +0900

    implements first sort =================== Emojis ====================

---
+++ src/first.rs
@@ -0,0 +1,26 @@
+pub fn sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up)
+    }
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], up);
+        sort(&mut x[mid_point..], up);
+    }
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i)
+        }
+    }
+}
